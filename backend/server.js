b5c16253858cb01d63cb24d7dda377a3075ebe2d
YannHyaric <ylhalmeida@gmail.com>
2020-11-21 18:23:14 -0300

    Loading and MessageBox

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import data from "./data.js";
+
+const app = express();
+
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading() {
+    return (
+        <div>
+            <i className="fa fa-spinner fs spin"></i>Loading
+        </div>
+    )
+}
